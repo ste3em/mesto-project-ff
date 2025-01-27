31f530dec0549d277c7fec836df98e13eca4b9d1
ste3em <stethaem@yandex.ru>
2025-01-27 14:19:25 +0100

    formating code

--- scripts/index.js
+++ scripts/index.js
@@ -14,69 +14,68 @@ const inputCardLink = formNewCard.querySelector(".popup__input_type_url");
 
 // @todo: Функция создания карточки
 function createCard(cardData) {
-    const cardElement = cardTemplate.cloneNode(true);
-    const cardImage = cardElement.querySelector(".card__image");
-    const cardTitle = cardElement.querySelector(".card__title");
-    const deleteButton = cardElement.querySelector(".card__delete-button");
-    const likeButton = cardElement.querySelector(".card__like-button");
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  const likeButton = cardElement.querySelector(".card__like-button");
 
-    cardTitle.textContent = cardData.name;
-    cardImage.src = cardData.link;
-    cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
 
-    deleteButton.addEventListener("click", () => {
-        deleteButton.closest(".card").remove();
-    });
-    
-    likeButton.addEventListener("click", () => {
-        likeButton.classList.toggle("card__like-button_active");
-    });
-    
-    cardImage.addEventListener("click", () => {
-        openPopupImage(cardData.name, cardData.link);
-    });
-    
-    return cardElement;
+  deleteButton.addEventListener("click", () => {
+    deleteButton.closest(".card").remove();
+  });
+
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("card__like-button_active");
+  });
+
+  cardImage.addEventListener("click", () => {
+    openPopupImage(cardData.name, cardData.link);
+  });
+
+  return cardElement;
 }
 
 // @todo: Вывести карточки на страницу
 function renderCards() {
-    placesList.innerHTML = "";
-    const savedCards = initialCards;
-    savedCards.forEach((card) => placesList.append(createCard(card)));
+  placesList.innerHTML = "";
+  const savedCards = initialCards;
+  savedCards.forEach((card) => placesList.append(createCard(card)));
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    
-    renderCards();
+  renderCards();
 });
 
 // @todo: Обработчики событий
 popupCloseButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-        closePopup(event.target.closest(".popup"));
-    });
+  button.addEventListener("click", (event) => {
+    closePopup(event.target.closest(".popup"));
+  });
 });
 
 formNewCard.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const newCard = {
-        name: inputCardName.value,
-        link: inputCardLink.value,
-    };
-    placesList.prepend(createCard(newCard));
-    
-    formNewCard.reset();
-    closePopup(popupNewCard);
+  event.preventDefault();
+  const newCard = {
+    name: inputCardName.value,
+    link: inputCardLink.value,
+  };
+  placesList.prepend(createCard(newCard));
+
+  formNewCard.reset();
+  closePopup(popupNewCard);
 });
 
 function openPopupImage(name, link) {
-    popupImageElement.src = link;
-    popupImageElement.alt = name;
-    popupCaption.textContent = name;
-    popupImage.classList.add("popup_is-opened");
+  popupImageElement.src = link;
+  popupImageElement.alt = name;
+  popupCaption.textContent = name;
+  popupImage.classList.add("popup_is-opened");
 }
 
 function closePopup(popup) {
-    popup.classList.remove("popup_is-opened");
+  popup.classList.remove("popup_is-opened");
 }
