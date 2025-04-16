4119a675291f53c580b18308b42d60721acb2472
ste3em <stethaem@yandex.ru>
2025-04-16 17:21:53 +0200

    ПР 6

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
-console.log('Hello, World!');
+import { initialCards } from './scripts/cards.js';
+import './pages/index.css';
+// import './vendor/font.css';
+// import './vendor/normalize.css';
+
+// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+
+// @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
+
+// @todo: Функция создания карточки
+// все переменные, которые не изменяются, объявлены через const
+function createCard(cardData, deleteCallback) {
+    const cardElement = cardTemplate.cloneNode(true).firstElementChild;
+    const cardImage = cardElement.querySelector(".card__image");
+    const cardTitle = cardElement.querySelector(".card__title");
+    const deleteButton = cardElement.querySelector(".card__delete-button");
+
+    cardTitle.textContent = cardData.name;
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+
+    deleteButton.addEventListener("click", function() {
+        deleteCallback(cardElement);
+    });
+    
+    return cardElement;
+}
+
+// @todo: Функция удаления карточки
+//функция удаления карточки теперь не зависит от родительского элемента placesList
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
+
+// @todo: Вывести карточки на страницу
+// вместо for используется метод массива .forEach()
+function renderCards() {
+    placesList.innerHTML = ""; // Очищаем список
+    initialCards.forEach(card => {
+        const cardElement = createCard(card, deleteCard);
+        placesList.appendChild(cardElement);
+    });
+}
+
+// @todo: Запуск функции при загрузке страницы
+//  устаревший DOMContentLoaded заменён на window.addEventListener("load", () => {})
+window.addEventListener("load", () => {
+    renderCards();
+});
